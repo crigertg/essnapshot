615708120c6fe1154d4397a31ce27c9de796998a
ctriegg <claus.riegg@gmail.com>
2020-08-07 15:16:15 +0200

    fix style issues in test_es.py

--- tests/unit/test_es.py
+++ tests/unit/test_es.py
@@ -1,18 +1,18 @@
-import pytest
 from essnapshot.es import initialize_es_client
 import elasticsearch
 
 
 def test_initialize_es_client_default():
-    assert isinstance(initialize_es_client(None), elasticsearch.client.Elasticsearch)
+    assert isinstance(initialize_es_client(None),
+                      elasticsearch.client.Elasticsearch)
 
 
 def test_initialize_es_client_with_params():
-#    with pytest.raises(TypeError):
     es_connections = [
         {
             'host': 'localhost',
             'port': '9200',
         }
     ]
-    assert isinstance(initialize_es_client(es_connections), elasticsearch.client.Elasticsearch)
+    assert isinstance(initialize_es_client(es_connections),
+                      elasticsearch.client.Elasticsearch)
