91063458812b317749fc9f7c9506a6c815ec25ca
gricertg <gricertg@gmail.com>
2020-08-18 17:49:37 +0200

    [#4] add documentation

--- essnapshot/cli.py
+++ essnapshot/cli.py
@@ -1,3 +1,4 @@
+"""This script is for running the esssnapshot utility interactivly."""
 from optparse import OptionParser
 from time import sleep
 from essnapshot.helpers import open_configfile, snapshot_name
@@ -7,6 +8,19 @@ import essnapshot.es as es
 
 
 def wait_for_running_snapshots(esclient, repository_name):
+    """Run in a loop until all running snapshots are done or failed.
+
+    Parameters
+    ----------
+    esclient : Elasticsearch
+        the client for the elasticsearch cluster to connect to.
+    repostiory_name : str
+        the name of the repository to check for running snapshots
+
+    Returns
+    -------
+    nothing
+    """
     while check_snapshots_in_progress(
         es.get_snapshots(esclient, repository_name)
     ):
@@ -15,7 +29,7 @@ def wait_for_running_snapshots(esclient, repository_name):
 
 
 def main():
-    # parse the only needed command line parameter
+    """Runs the essnapshot tool."""
     parser = OptionParser()
     parser.add_option("-c", "--config", dest="configfile",
                       help="Path to configuration file. "
@@ -24,15 +38,16 @@ def main():
                       metavar="FILE")
     options = parser.parse_args()[0]
 
-    # check if configfile parameter is given
     if options.configfile is None:
         parser.error('No configuration file given.')
 
-    # fetch config from configfile
     config = open_configfile(options.configfile)
 
+    # if the optional es_connections parameter is given, use it
+    # otherwise we set None to use the default config
     esconfig = config['es_connections'] if 'es_connections' in config else None
     esclient = es.initialize_es_client(esconfig)
+
     es.connection_check(esclient)
     es.ensure_snapshot_repo(
         esclient,
@@ -41,13 +56,11 @@ def main():
     wait_for_running_snapshots(esclient, config['repository_name'])
     es.create_snapshot(esclient, config['repository_name'], snapshot_name())
 
-    # find all snapshots to delete
     wait_for_running_snapshots
     delete_eligible_snapshots = find_delete_eligible_snapshots(
         es.get_snapshots(esclient, config['repository_name']),
         config['retention_time'])
 
-    # delete snapshots older than the configured retention time
     if len(delete_eligible_snapshots) > 0:
         es.delete_snapshots(esclient, config['repository_name'],
                             delete_eligible_snapshots)
